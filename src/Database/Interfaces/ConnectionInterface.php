c4b0408ba321d84b3fa5e92bf8db39c93045edea
AimForNaN <aimfornan@localhost>
2026-06-04 10:00:52 -0400

    Update ConnectionInterface.

--- src/Database/Interfaces/ConnectionInterface.php
+++ src/Database/Interfaces/ConnectionInterface.php
@@ -3,9 +3,12 @@
 namespace NaN\Database\Interfaces;
 
 use NaN\Database\Query\Builders\Interfaces\QueryBuilderInterface;
+use NaN\Database\Query\Statements\Interfaces\StatementInterface;
 
 interface ConnectionInterface {
 	public function close(): void;
+
+	public function exec(StatementInterface $query): mixed;
 
 	public function queryBuilder(): QueryBuilderInterface;
 
