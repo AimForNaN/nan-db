296fae48ecb59427e29564d478b95e8450817ac5
AimForNaN <aimfornan@localhost>
2026-07-12 07:43:21 -0400

    EntityTrait id property.

--- src/Database/Traits/EntityTrait.php
+++ src/Database/Traits/EntityTrait.php
@@ -8,6 +8,8 @@ use NaN\Database\Interfaces\EntityInterface;
  * @implements EntityInterface
  */
 trait EntityTrait {
+	private(set) string $id;
+
 	public static function fromArray(iterable $data): EntityInterface {
 		$new = new static();
 
